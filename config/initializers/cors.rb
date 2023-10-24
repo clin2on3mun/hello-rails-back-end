a9455cac5c2d47e1a8cb91e156761fd18a8a46c8
clin20n3mun <munanaclinton@gmail.com>
2023-10-24 16:26:24 +0200

    Add change to Cors to remove console error

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://localhost:5173'  # Remove the trailing slash
+      origins '*'  # Remove the trailing slash
   
       resource "*",
         headers: :any,
